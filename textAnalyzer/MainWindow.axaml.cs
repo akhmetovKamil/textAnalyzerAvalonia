8ce5836475402b0a72713efb4d56ae03e67fcf14
Kamil <alicesun65@gmail.com>
2023-10-15 15:46:57 +0300

    Refactor styles and add main event Handlers

--- textAnalyzer/MainWindow.axaml.cs
+++ textAnalyzer/MainWindow.axaml.cs
@@ -1,4 +1,6 @@
+using System;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace textAnalyzer;
 
@@ -6,6 +8,50 @@ public partial class MainWindow : Window
 {
     public MainWindow()
     {
+        // AnalyzeText.IsEnabled = false;
         InitializeComponent();
+    }
+
+    public string Text;
+    public bool IsFieldOpened = true;
+    public bool isFilePicked = false;
+    public bool CanAnalyzeText = false;
+    
+    private void AnalyzeText_OnClick(object? sender, RoutedEventArgs e)
+    {
+        СheckIsTextNotNull();
+        if (AnalyzeText.IsEnabled)
+        {
+            Text = IsFieldOpened ? Field.Text : "Run function which open file";
+        }
+        
+    }
+    
+    private void СheckIsTextNotNull()
+    {
+        if (IsFieldOpened) AnalyzeText.IsEnabled = string.IsNullOrEmpty(Field.Text);
+        else
+        {
+            // Проверять открыт ли файл
+        }
+    }
+    
+    private void BoolSwitcher(bool isField, bool isFile, bool isOpened)
+    {
+        FieldPanel.IsVisible = isField;
+        FilePanel.IsVisible = isFile;
+        IsFieldOpened = isOpened;
+    }
+    private void SwitchPanel_OnClick(object sender, RoutedEventArgs e) 
+    {
+        var whichButton = ((RadioButton)sender).Name;
+        if (whichButton == "FieldButton") BoolSwitcher(true, false, true);
+        else BoolSwitcher(false, true, false);
+    }
+
+    private void Field_OnTextChanged(object? sender, TextChangedEventArgs e)
+    {
+        AnalyzeText.IsEnabled = !string.IsNullOrEmpty(Field.Text);
+        CanAnalyzeText = !string.IsNullOrEmpty(Field.Text); // Delete variable
     }
 }
