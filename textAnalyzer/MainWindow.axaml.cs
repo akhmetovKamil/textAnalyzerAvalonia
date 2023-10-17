dcfe2e910681db8f17a269c2c24d355387e8c62d
Kamil <alicesun65@gmail.com>
2023-10-17 22:11:36 +0300

    Add file picker

--- textAnalyzer/MainWindow.axaml.cs
+++ textAnalyzer/MainWindow.axaml.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 
@@ -8,25 +10,40 @@ public partial class MainWindow : Window
 {
     public MainWindow()
     {
-        // AnalyzeText.IsEnabled = false;
         InitializeComponent();
     }
 
+
     public string Text;
     public bool IsFieldOpened = true;
-    public bool isFilePicked = false;
-    public bool CanAnalyzeText = false;
-    
-    private void AnalyzeText_OnClick(object? sender, RoutedEventArgs e)
+    public bool IsFilePicked = false;
+
+    private async void OpenFileButton_Click(object sender, RoutedEventArgs e)
+    {
+        // TODO Replace with TopLevel.StorageProvider
+        OpenFileDialog dialog = new OpenFileDialog();
+        dialog.AllowMultiple = false;
+        dialog.Filters.Add(new FileDialogFilter() { Name = "Text files", Extensions = { "txt" } });
+        var result = await dialog.ShowAsync(this);
+        if (result != null)
+        {
+            Text = await File.ReadAllTextAsync(result[0]);
+            FileName.Text = Path.GetFileName(result[0]);
+            IsFilePicked = true;
+            AnalyzeText.IsEnabled = true;
+        }
+        else AnalyzeText.IsEnabled = false;
+    }
+
+    private void AnalyzeText_OnClick(object sender, RoutedEventArgs e)
     {
         СheckIsTextNotNull();
         if (AnalyzeText.IsEnabled)
         {
             Text = IsFieldOpened ? Field.Text : "Run function which open file";
         }
-        
     }
-    
+
     private void СheckIsTextNotNull()
     {
         if (IsFieldOpened) AnalyzeText.IsEnabled = string.IsNullOrEmpty(Field.Text);
@@ -35,14 +52,15 @@ public partial class MainWindow : Window
             // Проверять открыт ли файл
         }
     }
-    
+
     private void BoolSwitcher(bool isField, bool isFile, bool isOpened)
     {
         FieldPanel.IsVisible = isField;
         FilePanel.IsVisible = isFile;
         IsFieldOpened = isOpened;
     }
-    private void SwitchPanel_OnClick(object sender, RoutedEventArgs e) 
+
+    private void SwitchPanel_OnClick(object sender, RoutedEventArgs e)
     {
         var whichButton = ((RadioButton)sender).Name;
         if (whichButton == "FieldButton") BoolSwitcher(true, false, true);
@@ -52,6 +70,5 @@ public partial class MainWindow : Window
     private void Field_OnTextChanged(object? sender, TextChangedEventArgs e)
     {
         AnalyzeText.IsEnabled = !string.IsNullOrEmpty(Field.Text);
-        CanAnalyzeText = !string.IsNullOrEmpty(Field.Text); // Delete variable
     }
 }
